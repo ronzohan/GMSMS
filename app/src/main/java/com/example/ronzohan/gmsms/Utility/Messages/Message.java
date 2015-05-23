f89bc933933972d7d30a42c10b628c65ba6c4b7c
Ron Daryl L. Magno <ron.zohan@yahoo.com>
2015-05-24 01:49:20 +0800

    Finished up message object

--- app/src/main/java/com/example/ronzohan/gmsms/Utility/Messages/Message.java
+++ app/src/main/java/com/example/ronzohan/gmsms/Utility/Messages/Message.java
@@ -1,9 +1,18 @@
 package com.example.ronzohan.gmsms.Utility.Messages;
 
+import com.example.ronzohan.gmsms.Utility.Recipients.Recipient;
+import java.util.ArrayList;
+
 /**
  * Created by ronzohan on 5/24/15.
  */
 public interface Message {
-    int SendMessage();
+    String getMessage();
+    void setMessage(String message);
 
+    ArrayList<Recipient> getRecipients();
+
+    void addRecipient(Recipient recipient);
+
+    int SendMessage();
 }
--- app/src/main/java/com/example/ronzohan/gmsms/Utility/Messages/SMSMesssage.java
+++ app/src/main/java/com/example/ronzohan/gmsms/Utility/Messages/SMSMesssage.java
@@ -2,21 +2,40 @@ package com.example.ronzohan.gmsms.Utility.Messages;
 
 import com.example.ronzohan.gmsms.Utility.Recipients.Recipient;
 
+import java.util.ArrayList;
+
 /**
  * Created by ronzohan on 5/24/15.
  */
 public class SMSMesssage implements Message {
-    private Recipient[] messageRecipeints;
+    private ArrayList<Recipient> messageRecipients;
+    private String message;
 
-    public Recipient[] getMessageRecipeints() {
-        return messageRecipeints;
+    public SMSMesssage(String message) {
+        this.message = message;
     }
 
-    public void setMessageRecipeints(Recipient[] messageRecipeints) {
-        this.messageRecipeints = messageRecipeints;
+    public SMSMesssage(String message, ArrayList<Recipient> messageRecipients) {
+        this.message = message;
+        this.messageRecipients = messageRecipients;
     }
 
-    @Override
+    public ArrayList<Recipient> getRecipients() {
+        return messageRecipients;
+    }
+
+    public void addRecipient(Recipient recipient) {
+        messageRecipients.add(recipient);
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+
     public int SendMessage() {
         return 0;
     }
