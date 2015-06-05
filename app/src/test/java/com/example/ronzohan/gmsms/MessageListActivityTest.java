03bd79464b3fc0783c72428aca9d88cbb9efd635
Ron Daryl L. Magno <ron.zohan@yahoo.com>
2015-06-06 00:18:42 +0800

    Added test cases

---
+++ app/src/test/java/com/example/ronzohan/gmsms/MessageListActivityTest.java
@@ -0,0 +1,57 @@
+package com.example.ronzohan.gmsms;
+
+import android.app.Activity;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.example.ronzohan.gmsms.Screens.MessageList.MessageListActivity;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import org.junit.runner.RunWith;
+
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+
+
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class)
+public class MessageListActivityTest {
+    private Activity messageListActivity;
+    private ListView messageListListView;
+
+    @Before
+    public void setUp() throws Exception{
+        messageListActivity = new MessageListActivity();
+        messageListActivity = Robolectric.setupActivity(MessageListActivity.class);
+        messageListListView = (ListView)messageListActivity.findViewById(R.id.message_list);
+    }
+
+    @Test
+    public void shouldDisplayTitle() throws Exception {
+        assertEquals(messageListActivity.getTitle().toString(), "Home");
+    }
+
+    @Test
+    public void listViewIsVisibleIfThereAreItems() throws Exception {
+        assertEquals(messageListListView.getVisibility(), View.VISIBLE);
+        assertNotEquals(messageListListView.getCount(), 0);
+    }
+
+    @Test
+    public void shouldDisplayMessageInfoActivityWhenListViewItemIsClicked() throws Exception {
+
+    }
+    // TODO - Implement this scenario
+    @Test
+    public void shouldDisplayEmptyIfListViewIsEmpty() throws Exception {
+
+    }
+}
+
---
+++ app/src/test/java/com/example/ronzohan/gmsms/Utility/SMSMessageTest.java
@@ -0,0 +1,50 @@
+package com.example.ronzohan.gmsms.Utility;
+
+import com.example.ronzohan.gmsms.BuildConfig;
+import com.example.ronzohan.gmsms.Utility.Messages.SMSMessage;
+import com.example.ronzohan.gmsms.Utility.Recipients.Recipient;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
+
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import java.util.ArrayList;
+
+public class SMSMessageTest {
+    SMSMessage smsMessageNoRecipient;
+    SMSMessage smsMessageWithRecipients;
+    int recipientCount;
+
+    @Before
+    public void setUp() {
+        Recipient firstRecipient = new Recipient("Ron Magno","09169777569");
+        recipientCount++;
+        Recipient secondRecipient = new Recipient("Clandine Salvedia","09355477233");
+        recipientCount++;
+
+        ArrayList<Recipient> recipientArrayList = new ArrayList<>();
+        recipientArrayList.add(firstRecipient);
+        recipientArrayList.add(secondRecipient);
+
+        smsMessageNoRecipient = new SMSMessage("What are you doing");
+        smsMessageWithRecipients = new SMSMessage("Whut u doin?", recipientArrayList);
+    }
+
+    @Test
+    public void addRecipientToSMSMessage() throws Exception {
+        Recipient firstRecipient = new Recipient("Zohan","09169777769");
+        smsMessageWithRecipients.addRecipient(firstRecipient);
+        recipientCount++;
+        assertEquals(smsMessageWithRecipients.getRecipients().size(), recipientCount);
+    }
+
+    // TODO -- implement this
+    @Test
+    public void shouldNotAddRecipientWithSameContactNo() throws Exception{
+
+    }
+}
