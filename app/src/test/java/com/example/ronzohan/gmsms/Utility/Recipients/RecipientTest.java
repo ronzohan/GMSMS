fb26285c18001c44a4109ec569281db266c15f87
Ron Daryl L. Magno <ron.zohan@yahoo.com>
2015-06-07 00:11:20 +0800

    Finished up recipient test

--- app/src/test/java/com/example/ronzohan/gmsms/Utility/Recipients/RecipientTest.java
+++ app/src/test/java/com/example/ronzohan/gmsms/Utility/Recipients/RecipientTest.java
@@ -1,7 +1,26 @@
 package com.example.ronzohan.gmsms.Utility.Recipients;
 
-/**
- * Created by ronzohan on 6/6/15.
- */
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class RecipientTest {
+    Recipient recipient;
+
+    @Before
+    public void setUp() throws Exception{
+        recipient = new Recipient("Ron Daryl Magno", "09169777569");
+    }
+
+    @Test
+    public void shouldReturnValues() throws Exception {
+        assertEquals(recipient.getContactNo(), "09169777569");
+        assertEquals(recipient.getName(), "Ron Daryl Magno");
+    }
+
+    @Test
+    public void shouldReturnBlankValues() throws Exception {
+        assertEquals(recipient.getAddress(), "");
+    }
 }
