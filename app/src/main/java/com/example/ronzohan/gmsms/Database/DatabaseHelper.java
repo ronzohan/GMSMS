78868133cf1119ccba31531379043860d64d0658
Ron Daryl L. Magno <ron.zohan@yahoo.com>
2015-06-23 19:35:48 +0800

    Added insert statements and revised table structure

--- app/src/main/java/com/example/ronzohan/gmsms/Database/DatabaseHelper.java
+++ app/src/main/java/com/example/ronzohan/gmsms/Database/DatabaseHelper.java
@@ -1,12 +1,17 @@
 package com.example.ronzohan.gmsms.Database;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
-public class DatabaseHandler extends SQLiteOpenHelper {
-    private static final int DATABASE_VERSION = 1;
+import com.example.ronzohan.gmsms.Utility.DaySchedule.DaySchedule;
+import com.example.ronzohan.gmsms.Utility.Messages.SMSMessage;
+import com.example.ronzohan.gmsms.Utility.Recipients.Recipient;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    private static final int DATABASE_VERSION = 2;
     private static final String DATABASE_NAME = "GMSMS";
 
     //Table Name
@@ -41,45 +46,46 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 
     String CREATE_MESSAGEINFO_TABLE = "CREATE TABLE " +  TABLE_MESSAGEINFO + "("
             + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," + COLUMN_MESSAGE + " TEXT,"
-            + COLUMN_TIME + " DATETIME," + COLUMN_DAYS_FK + " INTEGER, FOREIGN KEY ("
-            + COLUMN_MESSAGEINFO_FK +") REFERENCES " + TABLE_DAYS
-            + "(" + COLUMN_ID+ ")" + ");";
+            + COLUMN_TIME + " DATETIME" + ");";
 
     String CREATE_SIMASSIGNED_TABLE = "CREATE TABLE " + TABLE_SIMASSIGNED + "("
-            + COLUMN_ID + "INTEGER PRIMARY KEY AUTOINCREMENT," + COLUMN_SIMNAME + "TEXT,"
-            + COLUMN_MESSAGEINFO_FK + "INTEGER, FOREIGN KEY ("+ COLUMN_MESSAGEINFO_FK
+            + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," + COLUMN_SIMNAME + " TEXT,"
+            + COLUMN_MESSAGEINFO_FK + " INTEGER, FOREIGN KEY ("+ COLUMN_MESSAGEINFO_FK
             +") REFERENCES " + TABLE_MESSAGEINFO + "(" + COLUMN_ID+ ")" + ");";
 
     String CREATE_RECIPIENTS_TABLE = "CREATE TABLE " + TABLE_RECIPIENTS + "("
-            + COLUMN_ID + "INTEGER PRIMARY KEY AUTOINCREMENT," + COLUMN_CONTACTID + "INTEGER,"
-            + COLUMN_MESSAGEINFO_FK + "INTEGER, FOREIGN KEY ("+ COLUMN_MESSAGEINFO_FK
+            + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," + COLUMN_CONTACTID + " INTEGER,"
+            + COLUMN_MESSAGEINFO_FK + " INTEGER, FOREIGN KEY ("+ COLUMN_MESSAGEINFO_FK
             +") REFERENCES " + TABLE_MESSAGEINFO + "(" + COLUMN_ID+ ")" + ");";
 
-    String CREATE_DAYS_TABLE = "CREATE TABLE" + TABLE_DAYS + "("
-            + COLUMN_ID + "INTEGER PRIMARY KEY AUTOINCREMENT,"
+    String CREATE_DAYS_TABLE = "CREATE TABLE " + TABLE_DAYS + "("
+            + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
             + COLUMN_MONDAY + " INTEGER,"
             + COLUMN_TUESDAY + " INTEGER,"
             + COLUMN_WEDNESDAY + " INTEGER,"
             + COLUMN_THURSDAY + " INTEGER,"
             + COLUMN_FRIDAY + " INTEGER,"
             + COLUMN_SATURDAY + " INTEGER,"
-            + COLUMN_SUNDAY + " INTEGER" + ");";
+            + COLUMN_SUNDAY + " INTEGER,"
+            + COLUMN_MESSAGEINFO_FK + " INTEGER, FOREIGN KEY ("+ COLUMN_MESSAGEINFO_FK
+            +") REFERENCES " + TABLE_MESSAGEINFO + "(" + COLUMN_ID+ ")" + ");";
 
-    public DatabaseHandler(Context context) {
+    public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        SQLiteDatabase db = this.getWritableDatabase();
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(CREATE_MESSAGEINFO_TABLE);
         db.execSQL(CREATE_DAYS_TABLE);
+        db.execSQL(CREATE_MESSAGEINFO_TABLE);
         db.execSQL(CREATE_RECIPIENTS_TABLE);
         db.execSQL(CREATE_SIMASSIGNED_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        Log.w(DatabaseHandler.class.getName(),
+        Log.w(DatabaseHelper.class.getName(),
                 "Upgrading database from version " + oldVersion + " to "
                         + newVersion + ", which will destroy all old data");
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_MESSAGEINFO);
@@ -87,5 +93,30 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_RECIPIENTS);
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_SIMASSIGNED);
         onCreate(db);
+    }
+
+    public long insertMessageInfo(SMSMessage smsMessage) {
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_MESSAGE, smsMessage.getMessage());
+        values.put(COLUMN_TIME, smsMessage.getTime().toString());
+
+        return getWritableDatabase().insert(
+                TABLE_MESSAGEINFO,
+                null,
+                values);
+    }
+
+    public long insertDaySchedule(int messageInfoID, DaySchedule daySchedule) {
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_MONDAY, daySchedule.getMONDAY());
+        values.put(COLUMN_TUESDAY, daySchedule.getTUESDAY());
+        values.put(COLUMN_WEDNESDAY, daySchedule.getWEDNESDAY());
+        values.put(COLUMN_THURSDAY, daySchedule.getTHURSDAY());
+        values.put(COLUMN_FRIDAY, daySchedule.getFRIDAY());
+        values.put(COLUMN_SATURDAY, daySchedule.getSATURYDAY());
+        values.put(COLUMN_SUNDAY, daySchedule.getSUNDAY());
+        values.put(COLUMN_MESSAGEINFO_FK, messageInfoID);
+
+        return getWritableDatabase().insert(TABLE_DAYS, null, values);
     }
 }
