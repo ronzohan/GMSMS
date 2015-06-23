bc0566523e99219692c6cd765e9c9b7f1d2a38c8
Ron Daryl L. Magno <ron.zohan@yahoo.com>
2015-06-23 19:36:14 +0800

    Added getters and setters

---
+++ app/src/main/java/com/example/ronzohan/gmsms/Utility/DaySchedule/DaySchedule.java
@@ -0,0 +1,86 @@
+package com.example.ronzohan.gmsms.Utility.DaySchedule;
+
+public class DaySchedule {
+    int MONDAY = 0;
+    int TUESDAY = 0;
+    int WEDNESDAY = 0;
+    int THURSDAY = 0;
+    int FRIDAY = 0;
+    int SATURYDAY = 0;
+    int SUNDAY = 0;
+    int messageInfoID;
+
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getMONDAY() {
+        return MONDAY;
+    }
+
+    public void setMONDAY(int MONDAY) {
+        this.MONDAY = MONDAY;
+    }
+
+    public int getTUESDAY() {
+        return TUESDAY;
+    }
+
+    public void setTUESDAY(int TUESDAY) {
+        this.TUESDAY = TUESDAY;
+    }
+
+    public int getWEDNESDAY() {
+        return WEDNESDAY;
+    }
+
+    public void setWEDNESDAY(int WEDNESDAY) {
+        this.WEDNESDAY = WEDNESDAY;
+    }
+
+    public int getTHURSDAY() {
+        return THURSDAY;
+    }
+
+    public void setTHURSDAY(int THURSDAY) {
+        this.THURSDAY = THURSDAY;
+    }
+
+    public int getFRIDAY() {
+        return FRIDAY;
+    }
+
+    public void setFRIDAY(int FRIDAY) {
+        this.FRIDAY = FRIDAY;
+    }
+
+    public int getSATURYDAY() {
+        return SATURYDAY;
+    }
+
+    public void setSATURYDAY(int SATURYDAY) {
+        this.SATURYDAY = SATURYDAY;
+    }
+
+    public int getSUNDAY() {
+        return SUNDAY;
+    }
+
+    public void setSUNDAY(int SUNDAY) {
+        this.SUNDAY = SUNDAY;
+    }
+
+    public int getMessageInfoID() {
+        return messageInfoID;
+    }
+
+    public void setMessageInfoID(int messageInfoID) {
+        this.messageInfoID = messageInfoID;
+    }
+}
