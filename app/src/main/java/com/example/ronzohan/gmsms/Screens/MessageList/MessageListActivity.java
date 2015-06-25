313d27b5096f1ce8ad3ed2c27b7f8e614a980e76
Ron Daryl L. Magno <ron.zohan@yahoo.com>
2015-06-26 02:30:48 +0800

    Added call to database on app launch

--- app/src/main/java/com/example/ronzohan/gmsms/Screens/MessageList/MessageListActivity.java
+++ app/src/main/java/com/example/ronzohan/gmsms/Screens/MessageList/MessageListActivity.java
@@ -1,17 +1,18 @@
 package com.example.ronzohan.gmsms.Screens.MessageList;
 
-import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.app.FragmentManager;
 import android.support.v7.app.ActionBarActivity;
 import android.view.MenuItem;
 
+import com.example.ronzohan.gmsms.Database.DatabaseHelper;
 import com.example.ronzohan.gmsms.R;
 import com.crashlytics.android.Crashlytics;
 import io.fabric.sdk.android.Fabric;
 
 public class MessageListActivity extends ActionBarActivity {
+    DatabaseHelper dbHandler;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,8 +20,10 @@ public class MessageListActivity extends ActionBarActivity {
 
         setContentView(R.layout.activity_fragment);
         setTitle("Home");
-        FragmentManager fm = getFragmentManager();
 
+        dbHandler = new DatabaseHelper(this);
+
+        FragmentManager fm = getFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragment_container);
         if (fragment == null) {
             fragment = new MessageListFragment();
